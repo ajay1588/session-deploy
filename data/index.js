41e2a275518db30cbd959e69603e668658bdd02e
ajay <ajay.kumar1588@gmail.com>
2023-10-03 14:40:13 +0000

    deploy session

---
+++ data/index.js
@@ -0,0 +1,18 @@
+export const data = [
+
+  {
+      username:"ajay",
+      password:"abc123",
+      name:"Ajay 1588",
+      city: "Dehradun",
+      age: 30
+  },
+  {
+      username:"amol",
+      password:"xyz123",
+      name:"Amol 25",
+      city: "Delhi",
+      age: 25
+
+  }
+];
